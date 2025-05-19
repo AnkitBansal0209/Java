cb5099dfd1045afba61a559cac21016c51c6c396
AnkitBansal0209 <102477564+AnkitBansal0209@users.noreply.github.com>
2025-05-19 10:56:53 +0530

    Add files via upload

---
+++ FirstCode.java
@@ -0,0 +1,64 @@
+
+import java.util.*;
+public class FirstCode {
+    public static void main(String[] args){
+        //OUTPUT
+        //System.out.print("Hello World");
+
+        //INPUT
+ //      Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int sum = a + b;
+//        System.out.println(sum);
+
+        //CONDITIONALS
+//        Scanner sc = new Scanner(System.in);
+//        int age = sc.nextInt();
+//
+//        if (age>18){
+//            System.out.println("Adult");
+//        }
+//            else {
+//                System.out.println("Not Adult");
+//            }
+
+
+
+        //Switch
+
+//        Scanner sc = new Scanner(System.in);
+//        int button = sc.nextInt();
+//        switch(button){
+//            case 1 : System.out.println("Hello");
+//            break;
+//            case 2 : System.out.println("Namaste");
+//            break;
+//            case 3 : System.out.println("Bonjour");
+//            break;
+//            default : System.out.println("Not valid");
+//
+//        }
+
+        //PRINT NUMBERS FROM 1 TO 10
+//        Scanner sc = new Scanner(System.in);
+//        int i = sc.nextInt();
+//         for(int i = 1; i < 11 ; i++){
+//             System.out.println(i);
+//         }
+
+//        int i = 0;
+//        while( i<11){
+//            System.out.println(i);
+//             i++;
+//        }
+
+
+//        int i = 0;
+//        do{
+//            System.out.println(i);
+//            i = i+1;
+//        }
+//        while( i < 11);
+    }
+}
